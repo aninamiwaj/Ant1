b7dc58286293e5e7f9752eb4c8ad988f4b00287e
aninamiwaj <147338049+aninamiwaj@users.noreply.github.com>
2023-11-14 23:14:02 +0530

    Hello!

--- App.java
+++ App.java
@@ -1,13 +1,13 @@
 package com.companyname.bank;
 
 /**
- * Hello world!
+ * Hello!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello!" );
     }
 }
