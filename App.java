a3dbd128268ce509f226a41d1fd89a9d9e43246e
aninamiwaj <147338049+aninamiwaj@users.noreply.github.com>
2023-11-21 16:30:50 +0530

    polling change test

--- App.java
+++ App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "git hub trigger!" );
+        System.out.println( "polling change test!" );
     }
 }
