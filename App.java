6d062876f650f018fe8b8c8e5942c589e5702553
aninamiwaj <147338049+aninamiwaj@users.noreply.github.com>
2023-11-23 13:14:11 +0530

    Update App.java

--- App.java
+++ App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "github web hook triggring!" );
+        System.out.println( "Triggreing to Deployed!" );
     }
 }
